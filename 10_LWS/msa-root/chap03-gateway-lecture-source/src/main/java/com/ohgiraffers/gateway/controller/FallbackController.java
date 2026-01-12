5deff71823c7d5b14846e34de66a25242c895420
LimRaynor <morden9137@gmail.com>
2026-01-12 18:52:20 +0900

    msa- other,user 을 이용한 서버 응답확인

--- 10_LWS/msa-root/chap03-gateway-lecture-source/src/main/java/com/ohgiraffers/gateway/controller/FallbackController.java
+++ 10_LWS/msa-root/chap03-gateway-lecture-source/src/main/java/com/ohgiraffers/gateway/controller/FallbackController.java
@@ -9,6 +9,7 @@ public class FallbackController {
 
     @RequestMapping("/fallback/user")
     public Mono<String> userFallback() {
-        return Mono.just("현재 사용자 서비스에 장애가 발생했습니다. 잠시 후 다시 시도해주세요.");
+        return Mono.just
+                ("현재 사용자 서비스에 장애가 발생했습니다. 잠시 후 다시 시도해주세요.");
     }
 }
